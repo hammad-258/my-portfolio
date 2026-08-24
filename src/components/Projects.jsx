df023950b3e8fb1c0ed7f9ddd5e9b19f56a9a231
Hammad Aslam Khan <hammadaslamkhan2020@gmail.com>
2026-08-24 22:50:22 +0530

    modified:   src/components/Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,12 +23,14 @@ function ProjectCard({ title, description, tags, link }) {
   return (
     <div
       className="mat-short shadow-[10px_10px_0px_0px_rgba(35,35,35,1)] block border-8 border-white p-5 hover:rotate-[2deg] transition-all duration-300 ease-out">
+      <a href={link} target="_blank">
       <h3 className="font-semibold text-xl roboto-regular text-white mb-2">{title}</h3>
       <div  className="w-[28px]">
-        <a href={link} target="_blank"><svg height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg height="28px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        </svg></a>
+        </svg>
       </div>
+      </a>
       
       <p className="text-sm jetbrains-mono-regular text-gray-200 mb-4">{description}</p>
       <div className="flex flex-wrap gap-2">
